060d4b34873cd1ddd5e06e022589b12bf804ce7e
Ronnie2 <karonwebster@gmail.com>
2022-10-09 15:41:29 -0400

    Final Product TechJobs has Persisted

--- queries.sql
+++ queries.sql
@@ -1,7 +1,8 @@
 -- Part 1: Test it with SQL
-
+SELECT * FROM techjob_persist.employer;
 -- Part 2: Test it with SQL
-
+SELECT name FROM techjob_persist.employer where location = "St Louis City";
 -- Part 3: Test it with SQL
-
+drop table techjob_persist.job;
 -- Part 4: Test it with SQL
+select name from trechjob_persist.skill where id not in (select skills_id from job_skill);
--- src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
@@ -1,6 +1,8 @@
 package org.launchcode.techjobs.persistent.controllers;
 
 import org.launchcode.techjobs.persistent.models.Employer;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -13,6 +15,31 @@ import java.util.Optional;
 @RequestMapping("employers")
 public class EmployerController {
 
+    // thymeleaf Template must use class.repository.method/field
+    //event.employerRepository.name etc
+    //review annotations still a bit confusing 
+
+    @Autowired
+    private EmployerRepository employerRepository;
+
+    @GetMapping("")
+    public String index(@RequestParam(required = false)Integer employerId, Model model) {
+        if(employerId == null) {
+            model.addAttribute("employers", employerRepository.findAll());
+        }
+        else{
+            Optional<Employer> result = employerRepository.findById(employerId);
+            if(result.isEmpty()) {
+            model.addAttribute("employer","Invalid EmployerId "+ employerId);
+            }
+            else {
+                Employer employer = result.get();
+                model.addAttribute("employer","Items in Employer With Id: " + employer.getName());
+            }
+        }
+            return "employers/index";
+
+    }
 
     @GetMapping("add")
     public String displayAddEmployerForm(Model model) {
@@ -28,13 +55,16 @@ public class EmployerController {
             return "employers/add";
         }
 
+        employerRepository.save(newEmployer);
+          //  model.addAttribute("employer",employerRepository.findAll());
+
         return "redirect:";
     }
 
     @GetMapping("view/{employerId}")
     public String displayViewEmployer(Model model, @PathVariable int employerId) {
 
-        Optional optEmployer = null;
+        Optional optEmployer = employerRepository.findById(employerId);
         if (optEmployer.isPresent()) {
             Employer employer = (Employer) optEmployer.get();
             model.addAttribute("employer", employer);
